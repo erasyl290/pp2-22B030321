e4787e5c7fb1112dafd13f82c6ffba444e01afbc
erasyl290 <erasyl290@bk.ru>
2023-01-28 23:54:57 +0600

    Added _numbers.py

---
+++ tsis1/_numbers.py
@@ -0,0 +1,33 @@
+import random
+print(random.randrange(1, 10))
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# we can convert types with each other
+
+# int
+x = 1
+y = 35656222554887711
+z = -3255522
+
+# float
+x = 1.10
+y = 1.0
+z = -35.59
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+# complex
+x = 3+5j
+y = 5j
+z = -5j
