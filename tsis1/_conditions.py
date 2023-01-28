8fbbf1e097b9f9fb9f89fb49c6522d8523b4d17e
erasyl290 <erasyl290@bk.ru>
2023-01-29 02:28:13 +0600

    Added "_conditions.py"

---
+++ tsis1/_conditions.py
@@ -0,0 +1,15 @@
+a = 200
+b = 33
+
+if a > b: print("a is greater than b")
+
+print("A") if a > b else print("B")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+a = 33
+b = 200
+if b > a:
+  pass
