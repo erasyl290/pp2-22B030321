0b45ca268dead90ff852e4db15f4e8ff17a0fa95
erasyl290 <erasyl290@bk.ru>
2023-01-29 01:06:48 +0600

    Added "_strings.py"

---
+++ tsis1/_strings.py
@@ -0,0 +1,38 @@
+print("Hello")
+print('Hello')
+
+a = "Hello"
+print(a)
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
